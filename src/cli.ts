1127a8a5b79e9794431c5dc4dd9d34ac458ec08e
azu <azuciao@gmail.com>
2019-06-25 18:12:39 +0900

    feat(bin): add bin

---
+++ src/cli.ts
@@ -0,0 +1,44 @@
+import meow from "meow";
+import { createFromURL } from "./index";
+
+export function run() {
+    const cli = meow(`
+    Usage
+      $ create-security-alert-issue <github-security-alert-url>
+ 
+    Options
+      --dryRun Dry-Run when it is enabled
+      --token  GitHub Token, or support enviroment variables - GITHUB_TOKEN=xxx
+ 
+    Examples
+      $ create-security-alert-issue "https://github.com/azu/github-webhook-SecurityVulnerability-test/network/alert/package-lock.json/axios/open"
+`, {
+        flags: {
+            dryRune: {
+                type: "boolean"
+            }
+        },
+        autoHelp: true,
+        autoVersion: true
+    });
+
+    const token = process.env.GH_TOKEN || cli.flags.token;
+    if (!token) {
+        cli.showHelp(1);
+        return;
+    }
+    const promises = cli.input.map(url => {
+        return createFromURL(url, {
+            token: token,
+            dryRun: true
+        }).then(result => {
+            if (!result) {
+                return "";
+            }
+            return result.html_url;
+        });
+    });
+    return Promise.all(promises).then(issuesURL => {
+        return issuesURL.join("\n");
+    });
+}

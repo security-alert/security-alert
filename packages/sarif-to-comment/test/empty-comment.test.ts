ab5d2ec7a6028dc13dec0a5262ab54ae48062f47
gmarquet <gmarquet@talend.com>
2022-05-05 12:06:57 +0200

    Fix tests

--- packages/sarif-to-comment/test/empty-comment.test.ts
+++ packages/sarif-to-comment/test/empty-comment.test.ts
@@ -6,20 +6,27 @@ import { postComment } from "../src/index";
 
 console.log(__dirname);
 const sarifDir = path.join(__dirname);
-const actualFilePath = path.join(sarifDir, "/sarif.json");
+const actualFilePath = path.join(sarifDir, "sarif.json");
+
 describe("Testing", () => {
     it(`Testing failure due to empty sarif`, async function () {
         const actualOptions = {
-            sarifContent: JSON.parse(fs.readFileSync(actualFilePath).toString()),
-            postingURL: "http://localhost",
+            sarifContent: fs.readFileSync(actualFilePath, "utf-8"),
+            postingURL: "https://github.com/azu/security-alert/issues/1",
             sarifContentOwner: "aa",
             sarifContentRepo: "aa",
             sarifContentBranch: "aa",
             token: "aa"
         };
 
-        assert.throws(async () => {
-            await postComment(actualOptions);
-        }, "There are no results in this SARIF run 0, exiting without a comment !");
+        await assert.rejects(
+            async () => {
+                await postComment(actualOptions);
+            },
+            {
+                name: "Error", //  error type
+                message: "There are no results in this SARIF run 0, exiting without a comment !" // error message
+            }
+        );
     });
 });
